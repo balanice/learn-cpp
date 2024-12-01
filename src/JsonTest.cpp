b37d1455c4419a07bf92d5ae5712ad4c837afaa9
balanice <tokuu@outlook.com>
2024-12-01 09:48:10 +0800

    Remove cjson

--- src/JsonTest.cpp
+++ src/JsonTest.cpp
@@ -1,26 +1,7 @@
 #include "JsonTest.h"
 
-#include <cjson/cJSON.h>
-// #include <cjson/cJSON_Utils.h>
 #include <spdlog/spdlog.h>
 
 void TestJson()
 {
-    cJSON *a = cJSON_CreateObject();
-    cJSON_AddFalseToObject(a, "isA");
-    char *aStr = cJSON_Print(a);
-    cJSON_Delete(a);
-    spdlog::info("str: {}", aStr);
-
-    std::string s = "{\"a\":\"cc\"";
-    cJSON *sJ = cJSON_Parse(s.c_str());
-    if (sJ == nullptr)
-    {
-        spdlog::warn("parse str error");
-    }
-    else
-    {
-        spdlog::info("parse success");
-    }
-    cJSON_Delete(sJ);
 }
